b639a3b02ffbddb591c6f95c6c76ff6cdc089dbb
Keller Wilt <kellerw2000@gmail.com>
2015-05-01 22:09:58 +0000

    Removed some test files, that only were made to be run once to check cloud9's ability Made file tree easier to look at. Improved size system on word search (not done though)

--- filetests.py
+++
@@ -1,7 +0,0 @@
-text = open("texts.txt")
-line = text.readline()
-print line
-if line == "FC what do?":
-    print "Bubble HED-GP!"
-else:
-    print "panic"
--- newspaper_puzzles.py
+++ newspaper_puzzles.py
@@ -10,6 +10,7 @@ def word_scramble(words_input):
             i = i[0:random_letter] + i[random_letter+1:]
         words_scrambled.append(temp)
     return words_scrambled
+print 'Word Scramble:'
 print word_scramble(["maybe","this","works"])
 def word_search(words_input):
     temp_number1 = 0
@@ -20,7 +21,10 @@ def word_search(words_input):
         temp_number1 += len(i)
         if len(i)>temp_number2:
             temp_number2 = len(i)
-    size = temp_number1 / len(words_input) + abs(temp_number1-temp_number2)
+    print temp_number1
+    temp_number1 /= len(words_input)
+    size = temp_number1 + abs(temp_number1 - temp_number2)
+    print size
     temp_number2 = 0 
     temp_number1 = 0
     grid = []
@@ -29,10 +33,13 @@ def word_search(words_input):
     for i in range(0,size):
         for o in range(0,size):
             grid[i] += 'X'
+    for i in words_input:
+        word_direction = directions[random.randint(0,7)]
     print_board(grid)
 def print_board(grid):
     for i in grid:
         for o in i:
             print o,
         print ''
-print word_search(["maybe","this","works"])
+print 'Word Search:'
+print word_search(["maybe","this","works","but","maybe","not","because",'my','size','algorithm','might','be','flawed'])
