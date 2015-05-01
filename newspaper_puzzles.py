5a8064173d6cb9c31e6b64c2a5599f7daf17731d
Keller Wilt <kellerw2000@gmail.com>
2015-05-01 21:36:09 +0000

    Added grid system to word search, that currently prints X on everything due to lack of code for actual words

--- newspaper_puzzles.py
+++ newspaper_puzzles.py
@@ -12,12 +12,27 @@ def word_scramble(words_input):
     return words_scrambled
 print word_scramble(["maybe","this","works"])
 def word_search(words_input):
-    temp_number = 0
+    temp_number1 = 0
+    temp_number2 = 0
     temp_string = ''
     directions = ["up","upright","right","downright","down","downleft","left","upleft"]
     for i in words_input:
-        temp_number += len(i)
-    size = temp_number / len(words_input
-    temp_number = 0
-
+        temp_number1 += len(i)
+        if len(i)>temp_number2:
+            temp_number2 = len(i)
+    size = temp_number1 / len(words_input) + abs(temp_number1-temp_number2)
+    temp_number2 = 0 
+    temp_number1 = 0
+    grid = []
+    for i in range(0,size):
+        grid.append([])
+    for i in range(0,size):
+        for o in range(0,size):
+            grid[i] += 'X'
+    print_board(grid)
+def print_board(grid):
+    for i in grid:
+        for o in i:
+            print o,
+        print ''
 print word_search(["maybe","this","works"])
