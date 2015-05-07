391540ea451043819ad51cafd6308630f03783f7
Keller Wilt <kellerw2000@gmail.com>
2015-05-07 15:56:09 +0000

    Started adding stuff to help the program figure out where to put the words without ruining everything, so far no success.

--- newspaper_puzzles.py
+++ newspaper_puzzles.py
@@ -1,4 +1,5 @@
 import random
+import math
 def word_scramble(words_input):
     words_scrambled = []
     temp = ''
@@ -21,20 +22,62 @@ def word_search(words_input):
         temp_number1 += len(i)
         if len(i)>temp_number2:
             temp_number2 = len(i)
-    print temp_number1
     temp_number1 /= len(words_input)
-    size = temp_number1 + abs(temp_number1 - temp_number2)
+    size = temp_number1 + abs(temp_number1 - temp_number2) + math.sqrt(temp_number1) 
     print size
     temp_number2 = 0 
     temp_number1 = 0
     grid = []
+    size = int(size)+1
     for i in range(0,size):
         grid.append([])
     for i in range(0,size):
         for o in range(0,size):
-            grid[i] += 'X'
-    for i in words_input:
-        word_direction = directions[random.randint(0,7)]
+            grid[i] += ' '
+    for word in words_input:
+        safe=True
+        while safe:
+            safe = True
+            word_direction = directions[random.randint(0,1)]
+            if word_direction == "up":
+                startx = random.randint(0,size-1)
+                starty = random.randint(len(word)-1,size-1)
+                tempx  = startx
+                tempy  = starty
+                for i in range(0,len(word)):
+                    if grid[tempx][tempy] != ' ':
+                        if grid[tempx][tempy] == word[i]:
+                            tempy -= 1
+                        else:
+                            safe = False
+                            break
+                if safe:
+                    for i in range(0,len(word)):
+                        grid[startx][starty] = word[i]
+                        starty-=1
+                    safe = False
+            if word_direction == "upright":
+                startx = random.randint(0,size-len(word)-1)
+                starty = random.randint(len(word)-1,size-1)
+                tempx  = startx
+                tempy  = starty
+                for i in range(0,len(word)):
+                    if grid[tempx][tempy] != ' ':
+                        if grid[tempx][tempy] == word[i]:
+                            tempy -= 1
+                            tempx += 1
+                        else:
+                            safe = False
+                            break
+                    else:
+                        tempy-=1
+                        tempx+=1
+                if safe:
+                    for i in range(0,len(word)):
+                        grid[startx][starty] = word[i]
+                        starty-=1
+                        startx+=1
+                    safe = False
     print_board(grid)
 def print_board(grid):
     for i in grid:
@@ -42,4 +85,4 @@ def print_board(grid):
             print o,
         print ''
 print 'Word Search:'
-print word_search(["maybe","this","works","but","maybe","not","because",'my','size','algorithm','might','be','flawed'])
+print word_search(['hat','pat','hut','put','walrus'])
