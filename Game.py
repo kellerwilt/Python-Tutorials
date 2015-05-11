c8ca98387f8c2e02e2a702111c77e083f07aa0a6
Keller Wilt <kellerw2000@gmail.com>
2015-05-11 22:17:28 +0000

    Added more "rooms" to the game. I could probably shorten the code by making the options a dictionary somehow, because dictionaries (in python at least) use strings as keys, which could avoid the letter to number conversion needed for a list.

--- Game.py
+++ Game.py
@@ -13,8 +13,28 @@ while trapped:
     if answer == 'C':
         print "You dive through the mirror, and hurt your head. This mirror doesn't lead to the matrix"
     if answer == 'D': 
-        print 'They forgot to lock you in. You escape and roam free forever'
+        print 'They forgot to lock you in. You escape and enter a seemingly endless hallway.'
         trapped = False
+        temp = raw_input('Press enter to continue')
         break
     temp = raw_input('Press enter to continue')
-    
+in_hallway = True
+while in_hallway:
+    print 'You look down the hallway, and it seems to go on forever. Along the walls, are doors, each one different from the next. What do you do?'
+    print 'A: Walk down the hallway to see where it leads'
+    print 'B: Start trying doors to see where they go'
+    print 'C: Go back to the room to see if you missed something'
+    print 'D: Yell down the hall to see if anyone else is there'
+    answer=raw_input('Which option do you choose (a,b,c,d)').upper()
+    if answer == 'A':
+        print 'You wander aimlessly for 10 minutes. You begin to realize the doors are repeating themselves, and that you have been going in circles.'
+    if answer == 'B':
+        print "You start going through the doors, they all lead to dreams you have had in the past. You eventually get to a door covered in a zebra stripe pattern. You open it and it leads to a replica of your house. The model is so accurate it's uncanny. Every detail is correct. You enter, out of curiousity."
+        in_hallway = False
+        temp = raw_input('Press enter to continue')
+        break
+        
+    if answer == 'C':
+        print "You enter the room again, and look everywhere you can, but you don't notice anything you didn't see before."
+    if answer == 'D':
+        print 'Your voice echoes eerily around you. You wait, but hear nothing but your own panicked breath.'
