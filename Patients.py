f1db21800e545f148c78d1b2b901f5dcef5f1bdd
Keller Wilt <kellerw2000@gmail.com>
2015-06-04 21:03:51 +0000

    Fixed the last of the operators and commas without proper spaces

--- Patients.py
+++ Patients.py
@@ -31,7 +31,7 @@ class PatientsImporter:
                 self.age = raw_input("What is " + self.name + "'s age now?")
                 lines.append(self.name.title() + ", " + self.height + ", " + self.weight + ", " + self.age + ", " + str(today.year) + ", " + str(today.month) + ", " + str(today.day) + "\n") 
                 self.last_appointment = self.next_appointment
-                self.next_appointment = self.last_appointment.replace(month = (self.last_appointment.month + 5) % 12, year = self.last_appointment.year + (self.last_appointment.month + 5)/12)
+                self.next_appointment = self.last_appointment.replace(month = (self.last_appointment.month + 5) % 12, year = self.last_appointment.year + (self.last_appointment.month + 5) / 12)
                 self.until_next_appointment = self.next_appointment - today
                 self.patients.append(Patient(self.name, self.height, self.weight, self.age, self.last_appointment, self.next_appointment, self.until_next_appointment)) 
             else: 
@@ -41,7 +41,7 @@ class PatientsImporter:
         file = open(data, "w")
         file.writelines(lines) 
 class Patient:
-    def __init__(self, name, height, weight, age, last_appointment, next_appointment, until_next_appointment):  
+    def __init__(self, name, height, weight, age, last_appointment, next_appointment, until_next_appointment):
         self.name = name
         self.height = height
         self.weight = weight
@@ -54,19 +54,19 @@ class Patient:
         auth_token = "1756bdc8319de713187a53a552e3b2a6"
         client = TwilioRestClient(account_sid, auth_token)
         if self.until_next_appointment.days == 10: 
-            #message = client.messages.create(to="NaN",from_="+14253183498",body="Reminder: " + self.name + ", your appointment is in " + str(self.until_next_appointment) + " days!")
+            #message = client.messages.create(to = "NaN", from_ = "+14253183498", body = "Reminder: " + self.name + ", your appointment is in " + str(self.until_next_appointment) + " days!")
             return "Reminder: " + self.name + "'s appointment is in " + str(self.until_next_appointment.days) + " days!" 
         if self.until_next_appointment.days == 1: 
-            #message = client.messages.create(to = "NaN",from_ = "+14253183498",body = "Reminder: " + self.name + ", your appointment is tomorrow!") 
+            #message = client.messages.create(to = "NaN", from_ = "+14253183498", body = "Reminder: " + self.name + ", your appointment is tomorrow!") 
             return "Reminder: " + self.name + "'s appointment is tomorrow!" 
         if self.until_next_appointment.days == 0: 
-            #message = client.messages.create(to="NaN",from_="+14253183498",body="Reminder: " + self.name + ", your appointment is today!") 
+            #message = client.messages.create(to = "NaN", from_ = "+14253183498", body = "Reminder: " + self.name + ", your appointment is today!") 
             return 'Reminder: ' + self.name + "'s appointment is today!" 
     def __str__(self): 
-        info = "Name: "+str(self.name).title() + '\n'
-        info += "Height: "+str(self.height) + '\n'
-        info += "Weight: "+str(self.weight) + '\n'
-        info += "Age: "+str(self.age) + '\n'
+        info = "Name: " + str(self.name).title() + '\n'
+        info += "Height: " + str(self.height) + '\n'
+        info += "Weight: " + str(self.weight) + '\n'
+        info += "Age: " + str(self.age) + '\n'
         info += "Last appointment: " + str(self.last_appointment.year) + '-' + str(self.last_appointment.month) + '-' + str(self.last_appointment.day) + '\n'
         if self.until_next_appointment.days < 31: 
             info += "Upcoming appointment: " + str(self.next_appointment) + " in " + str(self.until_next_appointment.days) + " days."
